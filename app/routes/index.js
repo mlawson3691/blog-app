9710cc784d4c2bce904745172e647fd98d4cdcdf
Mark Lawson <mlawson3691@gmail.com>
2016-10-27 13:41:54 -0700

    limit posts on index and show more with Load More button

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  postsToShow: 1,
+
   model() {
-    return this.store.findAll('post');
+    return this.store.query('post',
+      {orderBy: 'date',
+      limitToFirst: this.get('postsToShow')
+    });
   },
 
   actions: {
@@ -10,6 +15,12 @@ export default Ember.Route.extend({
       var newPost = this.store.createRecord('post', params);
       newPost.save();
       this.transitionTo('index');
+    },
+    loadMore() {
+      var currentCount = this.get('postsToShow');
+      currentCount++;
+      this.set('postsToShow', currentCount);
+      this.refresh();
     }
   }
 });
